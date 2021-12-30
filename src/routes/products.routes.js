5fa60ac4606188ced7e8e955a3f5b9a6e09e7e1f
Johansy <johansymg@gmail.com>
2021-12-30 18:56:08 -0400

    Adding mongoose-paginate v2

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,7 +4,11 @@ const router = Router();
 const Product = require('../models/Product');
 
 router.get('/products', async (req, res) => {
-    const Products = await Product.find();
+    const { limit = 5, page = 1 } = req.query;
+    const Products = await Product.paginate({}, {
+        limit: parseInt(limit),
+        page: parseInt(page),
+    });
     res.json(Products);
 });
 
